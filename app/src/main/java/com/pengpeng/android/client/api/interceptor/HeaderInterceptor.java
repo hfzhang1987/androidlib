a5ab40a6e44f305b47cad68d64cf5bb981f560a4
hfzhang1987 <309316183@qq.com>
2017-02-13 15:30:26 +0800

    添加网络框架封装

--- app/src/main/java/com/pengpeng/android/client/api/interceptor/HeaderInterceptor.java
+++ app/src/main/java/com/pengpeng/android/client/api/interceptor/HeaderInterceptor.java
@@ -1,22 +1,33 @@
 package com.pengpeng.android.client.api.interceptor;
 
 import java.io.IOException;
-import java.util.Map;
 
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
 
+/**
+ * 若要求Retrofit2的接口中每个都要增加上headers，又不想做重复的事情，
+ * 可以使用这种方法来为每个request请求都设置上相同的请求头header。
+ * 修改请求头request headers主要是通过拦截器来实现，如果还有增加其他的header，可以再进行增加。
+ */
 public class HeaderInterceptor implements Interceptor {
-    public static String c = "";
-    public static String k = "";
-    private Map<String, Object> header;
-
-
+    /**
+     * 请求拦截器，修改请求header
+     */
     @Override
     public Response intercept(Chain chain) throws IOException {
-        Request request = chain.request();
-        Response response = chain.proceed(request);
-        return response;
+        Request request = chain.request()
+                .newBuilder()
+                .addHeader("Content-Type", "text/html; charset=UTF-8")
+                //                    .addHeader("Accept-Encoding", "*")
+                .addHeader("Connection", "keep-alive")
+                .addHeader("Accept", "*/*")
+                .addHeader("Access-Control-Allow-Origin", "*")
+                .addHeader("Access-Control-Allow-Headers", "X-Requested-With")
+                .addHeader("Vary", "Accept-Encoding")
+                //                    .addHeader("Cookie", "add cookies here")
+                .build();
+        return chain.proceed(request);
     }
 }
